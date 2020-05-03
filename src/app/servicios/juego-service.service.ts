6e30b7ccee220bc9efa3d29e7ab51736367e8992
Gabriel <gabrielivansaliba@gmail.com>
2020-05-03 02:25:35 -0300

    fix en constructor de JuegoAdivina

--- src/app/servicios/juego-service.service.ts
+++ src/app/servicios/juego-service.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Juego } from '../clases/Juego';
 import { JuegoAdivina } from '../clases/juego-adivina';
-import { MiHttpService } from './mi-http/mi-http.service'; 
+import { MiHttpService } from './mi-http/mi-http.service';
+import { FirebaseServiceService } from '../servicios/firebase-service.service'; 
 
 @Injectable()
 export class JuegoServiceService {
 
   peticion:any;
-  constructor( public miHttp: MiHttpService ) {
+  constructor(private firebaseService: FirebaseServiceService, public miHttp: MiHttpService ) {
     this.peticion = this.miHttp.httpGetO("http://localhost:3003");
 //    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
   }
@@ -32,12 +33,12 @@ export class JuegoServiceService {
 
     let miArray: Array<Juego> = new Array<Juego>();
 
-    miArray.push(new JuegoAdivina("Juego 1", false));
-    miArray.push(new JuegoAdivina("Pepe", true));
-    miArray.push(new JuegoAdivina("Juego 3", false));
-    miArray.push(new JuegoAdivina("Juego 4", false));
-    miArray.push(new JuegoAdivina("Juego 5", false));
-    miArray.push(new JuegoAdivina("Juego 6", false));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Juego 1", false));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Pepe", true));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Juego 3", false));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Juego 4", false));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Juego 5", false));
+    miArray.push(new JuegoAdivina(this.firebaseService, "Juego 6", false));
     return miArray;
   }
 
@@ -51,12 +52,12 @@ export class JuegoServiceService {
     })
     let promesa: Promise<Array<Juego>> = new Promise((resolve, reject) => {
       let miArray: Array<Juego> = new Array<Juego>();
-      miArray.push(new JuegoAdivina("JuegoPromesa 1", false,"promesa"));
-      miArray.push(new JuegoAdivina("PepePromesa", true));
-      miArray.push(new JuegoAdivina("JuegoPromesa 3", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 4", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 5", false));
-      miArray.push(new JuegoAdivina("JuegoPromesa 6", false));
+      miArray.push(new JuegoAdivina(this.firebaseService, "JuegoPromesa 1", false,"promesa"));
+      miArray.push(new JuegoAdivina(this.firebaseService, "PepePromesa", true));
+      miArray.push(new JuegoAdivina(this.firebaseService, "JuegoPromesa 3", false));
+      miArray.push(new JuegoAdivina(this.firebaseService, "JuegoPromesa 4", false));
+      miArray.push(new JuegoAdivina(this.firebaseService, "JuegoPromesa 5", false));
+      miArray.push(new JuegoAdivina(this.firebaseService, "JuegoPromesa 6", false));
       resolve(miArray);
     });
 
